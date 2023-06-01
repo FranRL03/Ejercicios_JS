d7d93fc0dd8a1da2ade6271bb36fdb5f44a7c913
Fran Ruiz <fjavierrule@gmail.com>
2023-06-01 09:18:18 +0200

    js

--- Ejercicios JS/js/Ejercicio 2.js
+++ Ejercicios JS/js/Ejercicio 2.js
@@ -1,7 +1,7 @@
 const boton = document.getElementById("btnGenerar");
 const body = document.querySelector("body");
 const colorNumber = document.querySelector(".colorNumber");
-// const colorAviso = document.querySelector("#aviso");
+
 console.log(btnGenerar, body);
 
 const generarColor = () => {
@@ -22,3 +22,30 @@ const setBackground = () => {
 }
 
 btnGenerar.addEventListener("click", setBackground);
+
+
+
+const botonCont = document.querySelector("#btnGenerar");
+let contador = 0;
+let contadorElemento = document.querySelector("#contador");
+
+const coloresGenerados = () => {
+    contador++;
+    return contador;
+}
+
+function actualizarContador() {
+    let cont = coloresGenerados();
+    contadorElemento.innerHTML = cont;
+}
+
+botonCont.addEventListener("click", actualizarContador);
+
+
+const botonLimpiar = document.getElementById("btnLimpiar");
+
+function limpiar() {
+    document.body.style.backgroundColor = "white";
+  }
+
+botonLimpiar.addEventListener("click", limpiar);
